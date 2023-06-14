2cdd86099bb8573d3883174fdfffa788b58f1e4a
John Bryan Macalinao <114970133+BryzMcln@users.noreply.github.com>
2023-06-14 18:18:13 +0800

    Update api_unittest.py

--- api_unittest.py
+++ api_unittest.py
@@ -17,5 +17,32 @@ class FlaskAppTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.get_data(as_text=True))
 
+    def testing_add_customer(self):
+        data = {
+            "company": "Konoha",
+            "first_name": "Otsutsuki",
+            "last_name": "Kaguya",
+            "job_title": "Self-Assistant",
+            "address": "69 grove ",
+            "city": "Puerto Rico"
+        }
+        response = self.app.post("/customers", json=data)
+        self.assertEqual(response.status_code, 201)
+
+    def testing_update_customer(self):
+        data = {
+            "company": "Company ZXC",
+            "first_name": "Gadsf",
+            "last_name": "mckn",
+            "job_title": "Project Manager",
+            "address": "8th Street",
+            "city": "colorado"
+        }
+        response = self.app.put("/customers/29", json=data)
+        self.assertEqual(response.status_code, 201)
+
+    def testing_the_delete(self):
+        response = self.app.delete("/customers/52")
+        self.assertEqual(response.status_code, 200)
 if __name__ == '__main__':
     unittest.main()
